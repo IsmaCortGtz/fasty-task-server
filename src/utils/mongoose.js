624acd76267ca1257fd9e95a4917a87764f3f0f0
IsmaCortGtz <ismacortgtz@gmail.com>
2024-08-24 11:55:34 -0600

    Remove function to count users

--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -1,13 +1,8 @@
 // Dependencies
 import mongoose from 'mongoose';
-import { User } from '../models/User/v1.js';
 
 export function connectDB () {
   mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Database connected!'))
     .catch(err => console.error(err));
-}
-
-export async function countUsers () {
-  return await User.countDocuments({});
 }
