632e0f221fb50a7af505d484804657713401f9ae
IsmaCortGtz <ismacortgtz@gmail.com>
2024-08-24 12:00:01 -0600

    Update server configuration and dependencies

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-// Dev dependencies
-import dotenv from 'dotenv';
-
 // Dependencies
 import path from 'path';
 import express from 'express';
 import cors from 'cors';
-import { connectDB, countUsers } from './src/utils/mongoose.js';
+import { connectDB } from './src/utils/mongoose.js';
 import { errorHandler, handler404 } from './src/middlewares/errorHandler.js';
 import combineURL from './src/utils/combineURL.js';
 
@@ -17,9 +14,8 @@ import taskRouter from './src/routes/task/index.js';
 import sessionRouter from './src/routes/session/index.js';
 
 // Constants
-dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 const corsConfig = { origin: ['http://localhost:5173', process.env.MY_FRONTEND_DEPLOY] };
 
 // Middlewares
@@ -27,12 +23,8 @@ connectDB(); // Start mongoose
 app.use(cors(corsConfig));
 app.use(express.json());
 
-// Endpoit to avoid free hostings to sleep (you need to send a request here)
-app.get('/api/verify', (req, res) => res.send('fasty-task-server'));
-app.get('/api/verify/db', async (req, res) => {
-  const users = await countUsers();
-  res.json({ users });
-});
+// Endpoit to check if the server is running.
+app.get('/api/health', (_, res) => res.json({ status: 'ok' }));
 
 // Routes
 app.use('/api/:version/user', userRouter);
@@ -44,14 +36,14 @@ app.use('/api/:version/session', sessionRouter);
 // Handle any other API route
 app.use('/api/*', handler404);
 
-// Handle front-end or static routes
+// Handle front-end and static files
+app.use(express.static('public'));
 app.use((req, res) => {
   // If env var for other frontend is not set, serve the default frontend
-  const redirectTo = process.env.MY_FRONTEND_DEPLOY_REDIRECT || process.env.MY_FRONTEND_DEPLOY;
+  const redirectTo = process.env.MY_FRONTEND_DEPLOY;
   if (!redirectTo) return res.sendFile(path.resolve('public/index.html'));
   return res.redirect(combineURL(redirectTo, req.url));
 });
-app.use(express.static('public'));
 
 // Error handler
 app.use(errorHandler);
